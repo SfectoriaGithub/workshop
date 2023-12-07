798f8cdceb0da531f092a62fa5195168754dd972
khalil kraiem <khalilmedkraiem@gmail>
2023-12-07 19:51:40 +0100

    first commit

---
+++ src/constants/dummyData.js
@@ -0,0 +1,64 @@
+export const dummy = [
+  {
+    id: 1,
+    image: require("../assets/images/products/bootstrap.png"),
+    title: "bootstrap",
+    description:
+      "Bootstrap is a front-end framework that includes CSS and JavaScript components for common web application interface elements. It includes styles for navigation elements, forms, and buttons",
+    price: 80,
+    status: "available",
+  },
+  {
+    id: 2,
+    image: require("../assets/images/products/MERN.jpg"),
+    title: "MERN",
+    description:
+      "The MERN stack is a web development framework made up of the stack of MongoDB, Express.js, React.js, and Nodejs. It is one of the several variants of the MEAN stack.",
+    price: 1500,
+    status: "available",
+  },
+  {
+    id: 3,
+    image: require("../assets/images/products/javaScript.jpg"),
+    title: "Javascript",
+    description:
+      "JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics",
+    price: 80,
+    status: "available",
+  },
+  {
+    id: 4,
+    image: require("../assets/images/products/html-css.jpg"),
+    title: "HTML&CSS",
+    description:
+      "Learn how to use HTML and CSS to make webpages. HTML is the markup language that you surround content with, to tell browsers about headings, lists, tables, and more. CSS is the stylesheet language that you style the page with, to tell browsers to change the color, font, layout, and more.",
+    price: 120,
+    status: "available",
+  },
+  {
+    id: 5,
+    image: require("../assets/images/products/react-js.jpg"),
+    title: "ReactJs",
+    description:
+      "React. js is built using JSX – A combination of JavaScript and XML. Elements are created using JSX, then use JavaScript to render them on your site. While React has a steep learning curve for a junior developer, it's quickly shaping into one of the most popular and in-demand JavaScript libraries.",
+    price: 300,
+    status: "avaialbe",
+  },
+  {
+    id: 6,
+    image: require("../assets/images/products/node.png"),
+    title: "Nodejs / ExpressJs",
+    description: "As an asynchronous event-driven JavaScript runtime, Node.js is designed to build scalable network applications. In the following 'hello world' example, many connections can be handled concurrently. Upon each connection, the callback is fired, but if there is no work to be done, Node.js will sleep.",
+    price: 300,
+    status: "avaialbe",
+  },
+  {
+    id: 7,
+    image: require("../assets/images/products/Full Stack Developer Career Program_Course Card copy.webp"),
+    title: "Full Stack",
+    description:
+      "Full stack technology refers to the entire depth of a computer system application, and full stack developers straddle two separate web development domains: the front end and the back end. The front end includes everything that a client, or site viewer, can see and interact with.",
+    price: 1500,
+    status: "avaiable",
+  },
+];
